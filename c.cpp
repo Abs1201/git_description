b376080514433dd81597fbf99f424d38d4a34465
김성훈 <kimtiger1201@naver.com>
2023-03-04 15:09:28 +0900

    git description

--- c.cpp
+++
@@ -1,4 +0,0 @@
-ccc
-
-git push -u 주소 branch
-git push -u origin main
---
+++ description.cpp
@@ -0,0 +1,39 @@
+ccc
+
+
+
+
+
+
+//설명
+작업폴더 --git add--> staging area --git commit-->repository
+
+fast-forward-merge
+git rebase(갠적으로 지금은 잘안쓸듯, 개념만 알아두기)
+3-way merge
+squash and merge // git merge --squash
+pull == fetch + merge
+
+//명령어
+git init
+
+git add wow.cpp
+git commit -m 'msg'
+git add .
+git status
+git log --all --oneline
+
+
+git branch
+git switch master
+git add .
+git commit -m 'msg'
+
+git log --oneline --all --graph
+
+git branch -d //delete (merge complete)
+git branch -D //delete (not merged)
+
+git push -u 주소 branch
+git push -u origin main
+
